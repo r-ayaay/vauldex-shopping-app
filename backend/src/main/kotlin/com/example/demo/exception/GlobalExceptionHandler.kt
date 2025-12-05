3340d60592b9ec9b2e994792bf730af9f0bcff59
Raymond Anthony Aya-ay <54580181+r-ayaay@users.noreply.github.com>
2025-12-05 09:20:51 +0800

    feat: add global exception handler

---
+++ backend/src/main/kotlin/com/example/demo/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,48 @@
+package com.example.demo.exception
+
+
+import com.example.demo.dto.ErrorResponse
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import kotlin.collections.joinToString
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception::class)
+    fun handleGenericException(
+        ex: Exception,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorResponse> {
+
+        val response = ErrorResponse(
+            status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            message = ex.message ?: "Something went wrong",
+            path = request.requestURI
+        )
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response)
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationException(
+        ex: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorResponse> {
+
+        val message = ex.bindingResult.fieldErrors
+            .joinToString(", ") { "${it.field}: ${it.defaultMessage}" }
+
+        val response = ErrorResponse(
+            status = HttpStatus.BAD_REQUEST.value(),
+            message = message,
+            path = request.requestURI
+        )
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response)
+    }
+}
